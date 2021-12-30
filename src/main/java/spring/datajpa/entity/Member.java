9297e2c1c46af027bce8232fc865abb512d85247
Harry <chlim428@gmail.com>
2021-12-30 23:30:42 +0900

    예제 도메인 모델 및 테스트코드 작성

--- src/main/java/spring/datajpa/entity/Member.java
+++ src/main/java/spring/datajpa/entity/Member.java
@@ -1,23 +1,33 @@
 package spring.datajpa.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = { "id", "username", "age" })
 public class Member {
 
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String username;
+
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
 
 }
