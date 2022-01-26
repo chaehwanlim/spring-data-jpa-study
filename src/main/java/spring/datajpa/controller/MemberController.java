81651325401e51b53e565396960ecf70b171c602
chaehwanlim <chlim428@gmail.com>
2022-01-26 23:17:09 +0900

    feat: pageable을 사용해 멤버 리스트 조회 api를 구현한다

--- src/main/java/spring/datajpa/controller/MemberController.java
+++ src/main/java/spring/datajpa/controller/MemberController.java
@@ -1,9 +1,12 @@
 package spring.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import spring.datajpa.dto.MemberDto;
 import spring.datajpa.entity.Member;
 import spring.datajpa.repository.MemberRepository;
 
@@ -28,9 +31,16 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(Member.builder().username("memberA").build());
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(Member.builder().username("user" + i).build());
+        }
     }
 
 }
--- src/main/java/spring/datajpa/dto/MemberDto.java
+++ src/main/java/spring/datajpa/dto/MemberDto.java
@@ -3,6 +3,7 @@ package spring.datajpa.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import spring.datajpa.entity.Member;
 
 @Getter
 @Builder
@@ -12,5 +13,11 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+        this.teamName = member.getTeam().getName();
+    }
 
 }
